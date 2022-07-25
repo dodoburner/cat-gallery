b27cb95ab36cd931f63241a750eb1bce966493d8
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-25 15:36:43 +0200

    Add header, nav-links and images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css'
+import catImage from './images/cat-551554_1280.jpg';
+
+const images = document.querySelectorAll('img');
+
+images.forEach(element => {
+  element.src = catImage;
+});
